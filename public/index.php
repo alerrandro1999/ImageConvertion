b2c504fd5b5b42b13ad102aa55eec59351220d0b
alerrandro borges <alerrandrokaton@gmail.com>
2023-05-04 17:00:46 -0300

    feat: botão copiar

--- public/index.php
+++ public/index.php
@@ -1,10 +1,11 @@
 <?php
-// error_reporting(E_ALL);
-// ini_set("display_errors","Off");
+error_reporting(E_ALL);
+ini_set("display_errors", "Off");
 require __DIR__ . '/../vendor/autoload.php';
 
 use \App\File\ImageToText;
 
+$textarea = '';
 if (isset($_FILES['image']) && !empty($_FILES['image'])) {
 
     if ($_FILES['image']['type'] == 'video/mp4') {
@@ -15,7 +16,7 @@ if (isset($_FILES['image']) && !empty($_FILES['image'])) {
         $obImageToText = new ImageToText($_FILES['image']['tmp_name']);
         $text = $obImageToText->getText(100);
         $textarea = '<div>
-                        <textarea name="" id="" cols="150" rows="30" style="width: 100%; height: 100%; font-family: monospace; text-align: center;">' . $text . '</textarea>
+                        <textarea id="textarea" cols="120" rows="25" style="width: 100%; height: 100%; font-family: monospace; text-align: center;">' . $text . '</textarea>
                     </div>';
     }
 }
@@ -32,7 +33,7 @@ if (isset($_FILES['image']) && !empty($_FILES['image'])) {
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100&display=swap" rel="stylesheet">
-    <link rel="icon" type="image/png" href="imagem/ascii.png"/>
+    <link rel="icon" type="image/png" href="imagem/ascii.png" />
     <meta name="author" content="Alerrandro Borges de Almeida">
     <meta name="description" content="site para converter imagens para ascii">
     <meta name="keywords" content="imagens, conversor, ascii, arte ASCII, arte de texto">
@@ -47,18 +48,29 @@ if (isset($_FILES['image']) && !empty($_FILES['image'])) {
             <img src="imagem/git.png" alt="git hub" width="250">
         </div>
         <p>Preferencialmente imagens de tom cinza.</p>
-        <div class="container-form">
-            <form action="" method="post" enctype="multipart/form-data">
-                <div>
-                    <input id="image" onsubmit="Checkfiles(this)" type="file" name="image" accept="image/*" required>
+        <form action="" method="post" enctype="multipart/form-data">
+            <div class="container-form">
+                <div class="container-input">
+                    <input id="image" onsubmit="Checkfiles(this)" type="file" name="image" accept="image/*" aria-label="imagem" required>
                 </div>
-                <button type="submit">Converter</button>
-                <?= $textarea ?? ''  ?>
-            </form>
-        </div>
+                <div class="container-button">
+                    <button type="submit">Converter</button>
+                    <!-- <button type="button" id="copiar">Copiar ASCII</button> -->
+                    <?= $textarea === '' ? '' : '<button type="button" id="copiar">Copiar ASCII</button>' ?>
+                </div>
+            </div>
+            <?= $textarea ?? ''  ?>
+        </form>
         <?= $erro ?? '' ?>
-
     </main>
 </body>
+
+<script>
+    document.getElementById("copiar").addEventListener("click", function() {
+        var textarea = document.getElementById("textarea");
+        textarea.select();
+        document.execCommand("copy");
+    });
+</script>
 
 </html>
